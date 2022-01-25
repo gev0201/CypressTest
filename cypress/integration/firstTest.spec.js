faf5d83ddad7d1f177238b37335bd9bf7c33e947
LAPTOP-VOLNLDP8\gevor <gevorg.gevorgyan@gmail.com>
2022-01-25 02:22:54 -0800

    change

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="Cypress" />
+
 // describe("Open guru99 Demo", () => {
 //     context("login", () => {
 //         it("aaaaaaaaa", () => {});
@@ -10,8 +11,23 @@
 //     cy.get('[name = "emailid"]')
 // })
 
-it.only("Open the Guru99 and register", () => {
-    cy.viewport(1800, 700)
-    cy.visit("https://demo.guru99.com/")
-})
+// it.only("Open the Guru99 and register", () => {
+//     // cy.viewport(1800, 700)
+//     cy.visit("https://demo.guru99.com/")
+// })
 
+it("Open the Guru99 and register", () => {
+    cy.visit("http://demo.guru99.com/")
+    cy.get("[name='emailid']")
+        .type("gev@mail.com")
+        // .should('have.value', "gev@mail.com")
+        // .and('be.visible')
+        .then( input=>{
+            expect(input).to.have.value("gev@mail.com")
+        })
+    cy.get("[name='btnLogin']").click()
+    cy.get("tbody").find('td').eq(2).then(function(txt){
+        cy.contains('Bank Project').click()
+        cy.get('[name="uid"]').type(txt.text())
+    })
+})
